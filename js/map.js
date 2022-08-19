d615f29a1499ac2f04e5e68879e440bfe99b05a8
0913vision <0913vision@gmail.com>
2022-08-20 07:17:31 +0900

    edit hint

--- js/map.js
+++ js/map.js
@@ -8,7 +8,7 @@ const handlePasswordSubmit = (e) => {
     e.preventDefault();
     const userInputData = userInput.value;
     if (userInputData === KEY) {
-        userResult.innerHTML = "영접기도문 비밀번호 ?71";
+        userResult.innerHTML = "영접기도문 비밀번호 ?7?";
     }
     else {
         userResult.innerHTML = "틀렸습니다.";
